4ffb12059fba813a365013d5bbe9f14e5b48ea85
harryzzbnn <genialtomail@gmail.com>
2025-08-09 20:35:41 +0200

    Update script.js

--- animated navigation/script.js
+++ animated navigation/script.js
@@ -2,3 +2,10 @@ const toggle = document.getElementById("toggle");
 const nav = document.getElementById("nav");
 
 toggle.addEventListener("click", () => nav.classList.toggle("active"));
+div {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
